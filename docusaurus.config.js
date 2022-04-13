b0bf527ea691aa062382bbce3dc1dfb7cc949d65
williamcollier <william.collier@bet365.com>
2022-04-13 14:37:41 +0100

    Add GitHub stars button.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Riak KV',
+  title: 'Riak',
   tagline: 'Documentation for Riak KV 2.2.3+',
   url: 'https://www.riak.github.io',
   baseUrl: '/',
