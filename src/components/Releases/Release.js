73fdd43e6acd6e870bcaf3a438ce4a43654992db
williamcollier <william.collier@bet365.com>
2022-04-13 14:37:20 +0100

    Re-organise file.

--- src/components/Releases/Release.js
+++ src/components/Releases/Release.js
@@ -1,31 +1,6 @@
 import React from 'react';
 import styles from './styles.module.css';
 
-export default function Release({ latest, first, name, tarball_url, zipball_url, html_url, tag_name }) {
-  const version_match = tag_name.match(/riak-([0-9]+).([0-9]+)(.)?([0-9]+)?p?([0-9]+)?/);
-  const latest_label = latest ? <Latest /> : <></>;
-  const github = <Link name="GitHub" url={html_url} />;
-  const packages_download = <PackagesDownload version_match={version_match} />;
-  const release_notes = <ReleaseNotes version_match={version_match} />;
-  const source_tgz = <Link name="source .tgz" url={tarball_url} />;
-  const source_zip = <Link name="source .zip" url={zipball_url} />;
-
-  return (
-    <>
-      <h1 className="anchor anchorWithStickyNavbar_node_modules-@docusaurus-theme-classic-lib-next-theme-Heading-styles-module" id={tag_name}>
-        {name}
-        <a className="hash-link" href={`#${tag_name}`} title="Direct link to heading">​</a>
-      </h1>
-      {latest_label}
-      <p>{github} &#124; {packages_download} &#124; {release_notes} &#124; {source_tgz} &#124; {source_zip}</p>
-    </>
-  );
-}
-
-function Link({ name, url }) {
-  return <a href={url}>{name}</a>;
-}
-
 function ReleaseNotes({ version_match }) {
   if (version_match === null) {
     return <>No release notes found</>;
@@ -68,4 +43,29 @@ function Hug() {
   const alt = "A new version of Riak has been released! Yay!"
 
   return <img src={require('@site/static/images/layout/hugs.png').default} alt={alt} title={alt} />;
+}
+
+function Link({ name, url }) {
+  return <a href={url}>{name}</a>;
+}
+
+export default function Release({ latest, first, name, tarball_url, zipball_url, html_url, tag_name }) {
+  const version_match = tag_name.match(/riak-([0-9]+).([0-9]+)(.)?([0-9]+)?p?([0-9]+)?/);
+  const latest_label = latest ? <Latest /> : <></>;
+  const github = <Link name="GitHub" url={html_url} />;
+  const packages_download = <PackagesDownload version_match={version_match} />;
+  const release_notes = <ReleaseNotes version_match={version_match} />;
+  const source_tgz = <Link name="source .tgz" url={tarball_url} />;
+  const source_zip = <Link name="source .zip" url={zipball_url} />;
+
+  return (
+    <>
+      <h1 className="anchor anchorWithStickyNavbar_node_modules-@docusaurus-theme-classic-lib-next-theme-Heading-styles-module" id={tag_name}>
+        {name}
+        <a className="hash-link" href={`#${tag_name}`} title="Direct link to heading">​</a>
+      </h1>
+      {latest_label}
+      <p>{github} &#124; {packages_download} &#124; {release_notes} &#124; {source_tgz} &#124; {source_zip}</p>
+    </>
+  );
 }
