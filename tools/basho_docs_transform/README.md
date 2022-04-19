9ac37846260fcc4294f4d022e74d5d444d3f4bfa
williamcollier <william.collier@bet365.com>
2022-04-19 09:52:45 +0100

    Make basho_docs_transform a normal directory. Add a transformer for converting code sections to docusaurus tabs.

---
+++ tools/basho_docs_transform/README.md
@@ -0,0 +1,35 @@
+# Why?
+
+This is used to transform markdown documents from [basho/basho_docs](https://github.com/basho/basho_docs). This script is specifically designed for the KV doc. As such, they have only been tested with the KV markdown files. Other markdown files may incidentally work. 
+
+The transformed markdown documents are then used in the overhauled Riak KV documentation that uses [Docusaurus](https://docusaurus.io/).
+
+# How to use
+
+You need some markdown files in a directory (the kv directory works fine). The output directory is then used in the Docusaurus website.
+
+```input_docs_dir``` and ```output_docs_dir``` must be an absolute paths
+
+```
+node index.js \--input_docs_dir=/home/USERNAME/Documents/kv \--output_docs_dir=/tmp/kv
+```
+
+# Transformation
+
+From the front matter, the title and identifier are stripped. If an identifier cannot be found, the title is lowercased and all spaces replaced with underscores.
+
+# Ignored files
+
+Drafts, 'redirect' files and files without a frontmatter are not included in the resulting ```output_docs_dir```. If the ```name_ignored_files``` flag is passed in, the filepath of ignored files are printed and the reason why they are ignored.
+
+```
+node index.js \--input_docs_dir=/home/USERNAME/Documents/kv \--output_docs_dir=/tmp/kv \--name_ignored_files
+```
+
+# Keeping Drafts
+
+If the ```keep_drafts``` is passed with a value of ```true``` any drafts will not be deleted. But they will **not** be transformed. By default, drafts are deleted. Drafts may or may not work with Docusaurus.
+
+```
+node index.js \--input_docs_dir=/home/USERNAME/Documents/kv \--output_docs_dir=/tmp/kv \--name_ignored_files \--keep_drafts=true
+```
---
+++ tools/basho_docs_transform/index.js
@@ -0,0 +1,186 @@
+const { resolve, extname, basename, dirname } = require('path');
+const { readdir, writeFile } = require('fs').promises;
+const { copySync, ensureDir, remove } = require('fs-extra');
+const util = require('util');
+const args = require('minimist')(process.argv.slice(2), { boolean: 'keep_drafts' });
+const yamlFront = require('yaml-front-matter');
+const remark = require('remark');
+const visit = require('unist-util-visit');
+const { shortcodes } = require('remark-hugo-shortcodes');
+
+const drafts = [];
+const redirects = [];
+const no_front_matters = [];
+
+async function* getMarkdownFiles(dir) {
+    const dirents = await readdir(dir, { withFileTypes: true });
+
+    for (const dirent of dirents) {
+        const f = resolve(dir, dirent.name);
+
+        if (dirent.isDirectory()) {
+            yield* getMarkdownFiles(f);
+        }
+        
+        if (extname(f) === '.md') {
+            const parsed = yamlFront.loadFront(f);
+            const no_front_matter = Object.keys(parsed).length <= 1;
+            const is_draft = parsed?.draft;
+            const is_redirect = parsed?.layout === 'redirect';
+
+            // Skip draft files (for now?), redirect files or files which only have content
+            if (no_front_matter || is_redirect || is_draft) {
+                if (no_front_matter) {
+                    no_front_matters.push(f);
+                }
+
+                if (is_redirect) {
+                    redirects.push(f);
+                }
+
+                if (is_draft) {
+                    drafts.push(f);
+
+                    if (args.keep_drafts) {
+                        continue;
+                    }
+                }
+
+                await remove(f);
+
+                continue;
+            }
+
+            yield { f, parsed };
+        }
+    }
+}
+
+function transformShortcodes(tree) {
+    return tree => {
+        visit(tree, 'shortcode', node => {
+            node.type = 'text';
+
+            if (node.closing) {
+                node.value = ':::';
+
+                return;
+            }
+
+            const title = node.attributes?.[0]?.[1] ?? '';
+            const title_fmt = ` ${title}`;
+
+            node.value = `:::note${title_fmt}`;
+        });
+    };
+}
+
+function transformCodeToTabs(tree) {
+    return tree => {
+        const repeated_code = [];
+
+        let num_sections = 0;
+
+        tree.children.forEach((child, i) => {
+            if (child.type !== 'code') {
+                if (repeated_code[num_sections] !== undefined) {
+                    num_sections++;
+                }
+
+                return;
+            }
+
+            repeated_code[num_sections] ??= [];
+
+            repeated_code[num_sections].push({ node_index: i, node: child });
+        });
+
+        const new_tree = repeated_code.map(repeated_section => {
+            const first_index = repeated_section[0].node_index;
+            const last_index = repeated_section[repeated_section.length - 1].node_index;
+            const nodes = repeated_section.map(({ node }) => node);
+            const transformed = [{ type: 'html', value: '<Tabs>' }];
+
+            nodes.forEach((node, i) => {
+                const node_value = node.value;
+                const lang = node.lang;
+                const label = lang !== null ? ` label="${lang[0].toUpperCase()}${lang.slice(1).toLowerCase()}"` : '';
+                const value = lang !== null ? ` value="${lang.toLowerCase()}"` : '';
+                const default_attribute = i === 0 ? ' default' : '';
+                const heading = `<TabItem${label}${value}${default_attribute}>`;
+                const opening = { type: 'html', value: heading };
+                const closing = { type: 'html', value: '</TabItem>' };
+
+                transformed.push(opening);
+
+                transformed.push(node);
+
+                transformed.push(closing);
+            });
+
+            transformed.push({ type: 'html', value: '</Tabs>' });
+
+            return { transformed, first_index, last_index };
+        });
+
+        let previous_section_end = 0;
+
+        const new_children = new_tree.map(({ transformed, first_index, last_index }) => {
+            const previous_sections = tree.children
+                .filter((_node, i) => i >= previous_section_end && i < first_index)
+
+            previous_section_end = last_index + 1;
+
+            return previous_sections.concat(transformed);
+        }).flat();
+
+        tree.children = new_children;
+    };
+}
+
+(async () => {
+    const output_docs_dir = args.output_docs_dir;
+    const shortcodeOptions = {
+        tokens: [['{{%', '%}}']],
+        inlineMode: true,
+        markdownAttributes: ['title']
+    };
+
+    await ensureDir(output_docs_dir);
+
+    copySync(args.input_docs_dir, output_docs_dir);
+
+    for await (const { f, parsed } of getMarkdownFiles(output_docs_dir)) {
+        const title = parsed.title;
+        const version = parsed.project_version;
+        const id = parsed.menu[`riak_kv-${version}`]?.identifier ?? title.toLocaleLowerCase().replace(/ /g, '_');
+        const content = parsed.__content;
+        const parsedContent = await remark()
+            .use(shortcodes, shortcodeOptions)
+            .use(transformShortcodes)
+            .use(transformCodeToTabs)
+            .process(content);
+        const output = `---\ntitle: ${title}\nid: ${id}\n---\n${parsedContent}`;
+
+        await writeFile(f, output);
+    }
+
+    if (args.name_ignored_files !== undefined) {
+        // Print full arrays
+        util.inspect.defaultOptions.maxArrayLength = null;
+
+        console.log('Drafts:');
+
+        console.log(drafts);
+
+        console.log('Redirects:');
+
+        console.log(redirects);
+
+        console.log('No Front Matters:');
+
+        console.log(no_front_matters);
+
+        console.log(`Num drafts: ${drafts.length}, Num redirects: ${redirects.length}, Num no front matter: ${no_front_matters.length}`);
+    }
+})();
