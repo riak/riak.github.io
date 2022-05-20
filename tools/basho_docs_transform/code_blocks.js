1612eb4a885e547885e9b596a9b92119ca7cb09d
williamcollier <william.collier@bet365.com>
2022-05-20 11:25:01 +0100

    Add code to format opening TabItem elements

--- tools/basho_docs_transform/code_blocks.js
+++ tools/basho_docs_transform/code_blocks.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const remark = require('remark');
 const visit = require('unist-util-visit');
+const config = require('./config.json');
 
 function addInsert(inserts, node, counter) {
   const new_counter = counter + 1;
@@ -10,12 +11,33 @@ function addInsert(inserts, node, counter) {
   return new_counter;
 }
 
+function formatLanguage(node_lang) {
+  if (node_lang === null) {
+    return { label: '', value: '' };
+  }
+
+  const lowered = node_lang.toLowerCase();
+  const config_name = config.languages[node_lang];
+  const lang = config_name !== undefined ? config_name : node_lang;  
+  const label = ` label="${lang[0].toUpperCase()}${lang.slice(1)}"`;
+  const value = ` value="${lang}"`;
+
+  return { label, value }; 
+}
+
+function createTabItem(node, is_first) {
+  const { label, value } = formatLanguage(node.lang);
+  const default_attribute = is_first ? ' default' : '';
+
+  return { type: 'html', value: `<TabItem${label}${value}${default_attribute}>` };
+}
+
 function transformCodeBlock() {
   const import_tab = { type: 'text', value: 'import Tabs from \'@theme/Tabs\';\n' };
   const import_tab_item = { type: 'text', value: 'import TabItem from \'@theme/TabItem\';\n' };
   const opening_tabs = { type: 'html', value: '<Tabs>' };
   const closing_tabs = { type: 'html', value: '</Tabs>' };
-  const opening_tab_item = { type: 'html', value: '<TabItem>' };
+  //const opening_tab_item = { type: 'html', value: '<TabItem>' };
   const closing_tab_item = { type: 'html', value: '</TabItem>' };
 
   return (tree) => {
@@ -46,6 +68,10 @@ function transformCodeBlock() {
 
       if (node.type === 'code' && previous?.type === 'code') {
         if (!is_sequential) {
+          // is_default as the previous node is a code but not marked as sequential 
+          // (i.e., the first code block in a sequence)
+          const opening_tab_item = createTabItem(previous, true);
+
           counter = addInsert(inserts, opening_tabs, counter);
           
           // Insert for previous entry
@@ -55,6 +81,8 @@ function transformCodeBlock() {
 
           is_sequential = true;
         }
+
+        const opening_tab_item = createTabItem(node, false);
 
         // Insert for current entry
         counter = addInsert(inserts, opening_tab_item, counter);
@@ -67,31 +95,11 @@ function transformCodeBlock() {
       counter = addInsert(inserts, closing_tabs, counter);
     }
 
-    const sorted = inserts.sort(({ index: a, index: b }) => b - a);
-
-    /*
-    const sorted = [
-      { index: 0, node: import_tab },
-      { index: 1, node: import_tab_item },
-      { index: 3, node: opening_tabs },
-        { index: 4, node: opening_tab_item },
-        { index: 6, node: closing_tab_item },
-        { index: 7, node: opening_tab_item },
-        { index: 9, node: closing_tab_item },
-      { index: 10, node: closing_tabs },
-      { index: 12, node: opening_tabs },
-        { index: 13, node: opening_tab_item },
-        { index: 15, node: closing_tab_item },
-        { index: 16, node: opening_tab_item },
-        { index: 18, node: closing_tab_item },
-      { index: 19, node: closing_tabs },
-    ];
-    */
-
-    sorted.forEach(({ index, node }) => tree.children.splice(index, 0, node));
+    inserts
+      .sort(({ index: a, index: b }) => b - a)
+      .forEach(({ index, node }) => tree.children.splice(index, 0, node));
   };
 }
-
 
 (async () => {
   const content = fs.readFileSync('codeblock.md', 'utf8');
