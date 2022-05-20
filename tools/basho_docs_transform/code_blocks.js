22520ae47ec9e5421db37e82951761dbe6427470
williamcollier <william.collier@bet365.com>
2022-05-20 10:39:10 +0100

    Fix > 2 code blocks being sequential to each other. Add imports.

--- tools/basho_docs_transform/code_blocks.js
+++ tools/basho_docs_transform/code_blocks.js
@@ -5,27 +5,36 @@ const visit = require('unist-util-visit');
 function addInsert(inserts, node, counter) {
   const new_counter = counter + 1;
 
-  //console.log({ node, str: `${counter} + 1 -> ${new_counter}` });
+  inserts.push({ index: new_counter, node });
 
-  inserts.push({ index: counter + 1, node });
-
-  return counter + 1;
+  return new_counter;
 }
 
 function transformCodeBlock() {
+  const import_tab = { type: 'text', value: 'import Tabs from \'@theme/Tabs\';\n' };
+  const import_tab_item = { type: 'text', value: 'import TabItem from \'@theme/TabItem\';\n' };
+  const opening_tabs = { type: 'html', value: '<Tabs>' };
+  const closing_tabs = { type: 'html', value: '</Tabs>' };
+  const opening_tab_item = { type: 'html', value: '<TabItem>' };
+  const closing_tab_item = { type: 'html', value: '</TabItem>' };
+
   return (tree) => {
     const inserts = [];
-    const opening_tabs = { type: 'html', value: '<Tabs>' };
-    const closing_tabs = { type: 'html', value: '</Tabs>' };
-    const opening_tab_item = { type: 'html', value: '<TabItem>' };
-    const closing_tab_item = { type: 'html', value: '</TabItem>' };
+    const insert_imports = tree.children.some(node => node.type === 'code');
 
+    if (insert_imports) {
+      // Always insert the imports on the first and second line
+      inserts.push({ index: 0, node: import_tab });
+
+      inserts.push({ index: 1, node: import_tab_item });
+    }
+
+    // Set counter 2 in order to skip the imports
+    let counter = 2;
     let is_sequential = false;
-    let counter = 0;
 
     for (const [i, node] of tree.children.entries()) {
-      const previous_index = i - 1;
-      const previous = tree.children[previous_index];
+      const previous = tree.children[i - 1];
 
       if (node.type !== 'code' && is_sequential) {
         is_sequential = false;
@@ -36,18 +45,16 @@ function transformCodeBlock() {
       }
 
       if (node.type === 'code' && previous?.type === 'code') {
-        console.log(`here: ${counter}`);
-
         if (!is_sequential) {
-          is_sequential = true;
-
           counter = addInsert(inserts, opening_tabs, counter);
+          
+          // Insert for previous entry
+          counter = addInsert(inserts, opening_tab_item, counter);
+
+          counter = addInsert(inserts, closing_tab_item, counter + 1);
+
+          is_sequential = true;
         }
-
-        // Insert for previous entry
-        counter = addInsert(inserts, opening_tab_item, counter);
-
-        counter = addInsert(inserts, closing_tab_item, counter + 1);
 
         // Insert for current entry
         counter = addInsert(inserts, opening_tab_item, counter);
@@ -60,26 +67,26 @@ function transformCodeBlock() {
       counter = addInsert(inserts, closing_tabs, counter);
     }
 
-    const sorted = inserts.sort(({ index: a, index: b }) => a - b);
+    const sorted = inserts.sort(({ index: a, index: b }) => b - a);
 
     /*
     const sorted = [
-      { index: 1, node: opening_tabs },
-        { index: 2, node: opening_tab_item },
-        { index: 4, node: closing_tab_item },
-        { index: 5, node: opening_tab_item },
-        { index: 7, node: closing_tab_item },
-      { index: 8, node: closing_tabs },
-      { index: 10, node: opening_tabs },
-        { index: 11, node: opening_tab_item },
-        { index: 13, node: closing_tab_item },
-        { index: 14, node: opening_tab_item },
-        { index: 16, node: closing_tab_item },
-      { index: 17, node: closing_tabs },
+      { index: 0, node: import_tab },
+      { index: 1, node: import_tab_item },
+      { index: 3, node: opening_tabs },
+        { index: 4, node: opening_tab_item },
+        { index: 6, node: closing_tab_item },
+        { index: 7, node: opening_tab_item },
+        { index: 9, node: closing_tab_item },
+      { index: 10, node: closing_tabs },
+      { index: 12, node: opening_tabs },
+        { index: 13, node: opening_tab_item },
+        { index: 15, node: closing_tab_item },
+        { index: 16, node: opening_tab_item },
+        { index: 18, node: closing_tab_item },
+      { index: 19, node: closing_tabs },
     ];
     */
-
-    console.log(sorted);
 
     sorted.forEach(({ index, node }) => tree.children.splice(index, 0, node));
   };
